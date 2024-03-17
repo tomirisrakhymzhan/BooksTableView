1e67085e7cb9dac645b41f3e64040db14fb71bc9
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2024-03-17 13:30:03 +0500

    second commit

---
+++ BookTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookTableView
+//
+//  Created by Томирис Рахымжан on 17/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
